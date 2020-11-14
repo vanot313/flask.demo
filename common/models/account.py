1a866f933053964fded14ab4d6ff1fe50a3311cb
Vanot <2834552723@qq.com>
2020-11-14 11:34:15 +0800

    更新服务器配置

--- common/models/account.py
+++ common/models/account.py
@@ -2,6 +2,8 @@ from application import db
 
 
 class Account(db.Model):
+    __tablename__ = 'account'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
