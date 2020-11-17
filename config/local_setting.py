c7266775d9bf4edac912868e61e6e90da5f70efc
Vanot <2834552723@qq.com>
2020-11-17 19:18:41 +0800

    database bug fixed

--- config/local_setting.py
+++ config/local_setting.py
@@ -1,8 +1,8 @@
 # coding:utf-8
-#本地开发配置文件
+# 本地开发配置文件
 from config.base_setting import *
-#这样的话就可以覆盖配置了
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://administrator:‘password’@49.235.73.129/mytest"
+# 这样的话就可以覆盖配置了
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://administrator:Jiang03.13@localhost/mytest"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_ENCODING = "utf-8"
