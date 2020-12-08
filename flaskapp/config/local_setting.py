b40efa238b90c0a9f4d954fdc908e74b331f8d00
root <root@localhost.localdomain>
2020-12-08 14:44:52 +0800

    docker-compose config update.up to server

--- flaskapp/config/local_setting.py
+++ flaskapp/config/local_setting.py
@@ -2,7 +2,7 @@
 # 本地开发配置文件
 from config.base_setting import *
 # 这样的话就可以覆盖配置了
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://administrator:Jiang03.13@localhost/mytest"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://administrator:Jiang03.13@49.235.73.129/mytest"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_ENCODING = "utf-8"
