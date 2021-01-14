8f648510f281f36929dbd408e1d7177628ad083d
Vanot <49399743+vanot313@users.noreply.github.com>
2021-01-14 20:46:59 +0800

    修正

--- flaskapp/services/companyDataService.py
+++ flaskapp/services/companyDataService.py
@@ -1,13 +1,25 @@
 # coding:utf-8
+import json
+
 import pandas as pd
 import numpy as np
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
 
 def fenxi(id):
     ID = id
-    
+
     data = pd.read_csv("uploadfile/verify.csv")
     x = data.iloc[:, data.columns != "flag"]
     y = data.iloc[:, data.columns == "flag"]
@@ -16,7 +28,7 @@ def fenxi(id):
     mapstrategy2 = {10: '有限责任公司', 20: '合伙企业',  30: '股份有限公司', 40: '农民专业合作社',  50: '集体所有制企业'}
     mapstrategy3 = {10: '自然人', 20: '企业法人'}
 
-    df = pd.read_csv("uploadfile/verify.csv")
+    df = pd.read_csv("uploadfile/test.csv")
     df = df.iloc[:, df.columns != "flag"]
     fenleiqi = RandomForestClassifier(n_estimators=47, random_state=20)
     fenleiqi = fenleiqi.fit(x, y.astype('int').values.ravel())
@@ -26,7 +38,7 @@ def fenxi(id):
     elif flag == 0:
         flag = "非僵尸企业"
 
-    df = pd.read_csv("uploadfile/verify.csv", index_col='ID')
+    df = pd.read_csv("uploadfile/test.csv", index_col='ID')
 
     try:
         industry = mapstrategy1[np.array(df.loc[[ID], ['行业']])[0][0]]
@@ -86,43 +98,44 @@ def fenxi(id):
 
     dictt = {}
 
-    dictt['id'] = ID
-    dictt['industry'] = industry  # 行业
-    dictt['comType'] = comType  # 企业类型
-    dictt['personType'] = personType  # 控制人类型
+    dictt['id'] = float(ID)
+    dictt['industry'] = str(industry)  # 行业
+    dictt['comType'] = str(comType)  # 企业类型
+    dictt['personType'] = str(personType)  # 控制人类型
 
-    dictt['regTime'] = regTime  # 注册时间
-    dictt['regMoney'] = regMoney  # 注册资金
-    dictt['rate'] = rate  # 控制人控股比例
+    dictt['regTime'] = str(regTime)  # 注册时间
+    dictt['regMoney'] = str(regMoney)  # 注册资金
+    dictt['rate'] = str(rate)  # 控制人控股比例
 
-    dictt['fengxian'] = fengxian  # 经营风险
-    dictt['yilai'] = yilai  # 政府依赖
-    dictt['zhouzhuan'] = zhouzhuan  # 资金周转
-    dictt['fuzhai'] = fuzhai  # 资产负债率
-    dictt['flag'] = flag  # 是否为僵尸企业
+    dictt['fengxian'] = str(fengxian)  # 经营风险
+    dictt['yilai'] = str(yilai)  # 政府依赖
+    dictt['zhouzhuan'] = str(zhouzhuan)  # 资金周转
+    dictt['fuzhai'] = str(fuzhai)  # 资产负债率
+    dictt['flag'] = str(flag)  # 是否为僵尸企业
 
-    dictt['rongzi_2015'] = rongzi_2015  # 融资额度
-    dictt['rongzi_2016'] = rongzi_2016
-    dictt['rongzi_2017'] = rongzi_2017
+    dictt['rongzi_2015'] = str(rongzi_2015)  # 融资额度
+    dictt['rongzi_2016'] = str(rongzi_2016)
+    dictt['rongzi_2017'] = str(rongzi_2017)
 
-    dictt['rongzicb_2015'] = rongzicb_2015  # 融资成本
-    dictt['rongzicb_2016'] = rongzicb_2016
-    dictt['rongzicb_2017'] = rongzicb_2017
+    dictt['rongzicb_2015'] = str(rongzicb_2015)  # 融资成本
+    dictt['rongzicb_2016'] = str(rongzicb_2016)
+    dictt['rongzicb_2017'] = str(rongzicb_2017)
 
-    dictt['zonghe_2015'] = zonghe_2015  # 综合资金成本
-    dictt['zonghe_2016'] = zonghe_2016
-    dictt['zonghe_2017'] = zonghe_2017
+    dictt['zonghe_2015'] = str(zonghe_2015)  # 综合资金成本
+    dictt['zonghe_2016'] = str(zonghe_2016)
+    dictt['zonghe_2017'] = str(zonghe_2017)
 
-    dictt['zichan_2015'] = zichan_2015  # 资产总额
-    dictt['zichan_2016'] = zichan_2016
-    dictt['zichan_2017'] = zichan_2017
+    dictt['zichan_2015'] = str(zichan_2015)  # 资产总额
+    dictt['zichan_2016'] = str(zichan_2016)
+    dictt['zichan_2017'] = str(zichan_2017)
 
-    dictt['fuzhai_2015'] = fuzhai_2015  # 负债总额
-    dictt['fuzhai_2016'] = fuzhai_2016
-    dictt['fuzhai_2017'] = fuzhai_2017
+    dictt['fuzhai_2015'] = str(fuzhai_2015) # 负债总额
+    dictt['fuzhai_2016'] = str(fuzhai_2016)
+    dictt['fuzhai_2017'] = s    tr(fuzhai_2017)
 
     di.append(dictt)
     dict["data"] = di
+
     return dict
 
 
@@ -133,7 +146,7 @@ def multiple():
 
     test = pd.read_csv('uploadfile/test.csv')
     Xtest = test.iloc[:, test.columns != "flag"]
-    Ytest = test.iloc[:, test.columns == "flag"]
+    Ytest = test.iloc[:, test   .columns == "flag"]
 
     fenleiqi = RandomForestClassifier(n_estimators=47, random_state=20)
     fenleiqi = fenleiqi.fit(x, y.astype('int').values.ravel())
