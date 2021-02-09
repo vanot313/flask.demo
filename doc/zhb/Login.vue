031162dc54922fd8eced6d07131b3962564adbcf
zzzh1345 <62043483+zzzh1345@users.noreply.github.com>
2021-02-09 21:43:04 +0800

    更新

---
+++ doc/zhb/Login.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!--图标-->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt/>
+      </div>
+      <!--表单区域-->
+      <el-form :model="ruleForm" :rules="rules" ref="loginForm" label-width="0" class="login_form">
+        <!--用户名-->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icongithub"></el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont iconlock"></el-input>
+        </el-form-item>
+        <!--按钮-->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="submitForm('ruleForm')">立即登录</el-button>
+          <el-button type="info" @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  rules: {
+    username: [
+      { required: true, message: '请输入活动名称', trigger: 'blur' },
+      { min: 3, max: 6, message: '长度在 3 到 6 个字符', trigger: 'blur' }
+    ],
+    password: [
+      { required: true, message: '请输入密码', trigger: 'blur' },
+      { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+    ]
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+<style scoped>
+.login_container{
+  /*根节点样式*/
+  background-color: pink;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;/*圆角*/
+  position: absolute;/*绝对定位*/
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
+.login_box .avatar_box{
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background-color: #0ee;
+}
+.login_box .avatar_box img{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form{
+  position: absolute;
+  bottom: 0%;
+  width: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
+}
+</style>
